3fc1f726044577006b9d6383ded68698206331a6
Ren <javaandnet@gmail.com>
2023-03-27 21:47:14 +0900

    增加读取Menu

--- src/sfwecom/routes/sf.js
+++ src/sfwecom/routes/sf.js
@@ -22,4 +22,23 @@ router.get('/file/:id', function (req, res, next) {
     var file =  jsh.file(req, res,req.params.id);
 });
 
+router.post('/menus', function (req, res, next) {
+  var jsh = new JSH();
+  var file =  jsh.query("SELECT Name, text__c, Url__c, Role__c, Image__c, Sort__c, Delete__c FROM MGMenu__c where Delete__c = false order by Sort__c").then(function(rtn){
+    var menus = rtn.records.map(menu=>{
+        return {
+          no:menu.Name,
+          text:menu.Text__c,
+          url:menu.Url__c,
+          role:menu.Role__c,
+          image:menu.Image__c,
+          sort:menu.Sort__c
+        };
+    });
+    var rtn = {data:menus};
+    res.send(rtn);
+
+  });
+});
+
 export default router;
